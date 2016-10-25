2545a4911635c268e1b39c22b50f08d3dc41c54e
Pierre-Nick <npierre2@students.kennesaw.edu>
2016-10-24 22:03:29 -0400

    Added Transitions and fundamental features

--- src/Assignment4/EmailActivity.java
+++ src/Assignment4/EmailActivity.java
@@ -1,12 +1,16 @@
 package Assignment4;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import com.sun.xml.internal.org.jvnet.mimepull.MIMEMessage;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -16,6 +20,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,11 +38,9 @@ public class EmailActivity extends Application {
     private final Label fromlbl = new Label("From");
     private final Label subjectlbl = new Label("Subject");
     private final Label messagelbl = new Label("Message");
-    private final Label attachmentlbl = new Label("Attachment");
-    private final Label incorrectlbl = new Label("Invalid Credentials!");
+    private final Label notiflbl = new Label("Invalid Credentials!");
     /* Button Declarations/Init */
     private final Button signInBtn = new Button("Sign-In");
-    private final Button forgetPassBtn = new Button("Forgot-Password");
     private final Button sendBtn = new Button("Send");
     private final Button closeBtn = new Button("Close");
     private final Button helpBtn = new Button("Help");
@@ -48,19 +51,20 @@ public class EmailActivity extends Application {
     private final TextField tfTo = new TextField();
     private final TextField tfFrom = new TextField();
     private final TextField tfSubject = new TextField();
+    private final TextArea taMessage = new TextArea();
     /* Fade-In Transitions */
-    private final FadeTransition fadeInSlow = new FadeTransition(new Duration(6000));
     private final FadeTransition fadeIn = new FadeTransition(new Duration(3000));
-    private final FadeTransition fadeInFast = new FadeTransition(new Duration(1000));
 
     @Override
     public void start(Stage primaryStage) {
         mainlbl.setPadding(new Insets(10));
-        VBox titleVbox = new VBox(mainlbl);
-        titleVbox.setAlignment(Pos.CENTER);
+        VBox titleVBox = new VBox(mainlbl);
+        titleVBox.setAlignment(Pos.CENTER);
+        titleVBox.getChildren().add(notiflbl);
+        notiflbl.setVisible(false);
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setTop(titleVbox);
+        borderPane.setTop(titleVBox);
         borderPane.setCenter(signInWindow());
 
 
@@ -71,15 +75,25 @@ public class EmailActivity extends Application {
 
         signInBtn.setOnAction(event -> {
             if (credentialsCheck()) {
-                sendEmailWindow(borderPane);
+                notiflbl.setVisible(false);
                 stageResize(primaryStage);
+                sendEmailWindow(borderPane);
             } else {
-                titleVbox.getChildren().add(incorrectlbl);
-                incorrectlbl.setTextFill(Color.RED);
+                notiflbl.setTextFill(Color.RED);
+                notiflbl.setVisible(true);
             }
+        });
+        sendBtn.setOnAction(event -> sendEmail());
+        closeBtn.setOnAction(event -> {
+            Platform.exit();
+            System.exit(0);
         });
     }
 
+    /**
+     * Generates the default sign-in window
+     * @return
+     */
     private VBox signInWindow() {
         GridPane guiGridPane = new GridPane();
         guiGridPane.setAlignment(Pos.CENTER);
@@ -95,16 +109,65 @@ public class EmailActivity extends Application {
         HBox buttonHbox = new HBox();
         buttonHbox.setAlignment(Pos.CENTER);
         buttonHbox.setSpacing(20);
-        buttonHbox.getChildren().addAll(signInBtn, forgetPassBtn);
+        buttonHbox.getChildren().addAll(signInBtn, closeBtn);
 
         return new VBox(15, guiGridPane, buttonHbox);
     }
 
-    private VBox sendEmailWindow(BorderPane borderPane) {
+    /**
+     * Takes in a borderPane as an argument.
+     * Clears the borderPane of its' contents and
+     * generates the email client.
+     * @param borderPane
+     */
+    private void sendEmailWindow(BorderPane borderPane) {
+        tfFrom.setText(tfEmail.getText());
+        tfFrom.setEditable(false);
         borderPane.getChildren().clear();
-        return new VBox();
+
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(10);
+        gridPane.setHgap(5);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.add(tolbl, 0, 0);
+        gridPane.add(tfTo, 1, 0);
+        gridPane.add(fromlbl, 0, 1);
+        gridPane.add(tfFrom, 1, 1);
+        gridPane.add(subjectlbl, 0, 2);
+        gridPane.add(tfSubject, 1, 2);
+        gridPane.add(messagelbl, 0, 4);
+        taMessage.setWrapText(true);
+        gridPane.add(taMessage, 1, 4);
+
+        HBox buttonHBox = new HBox();
+        buttonHBox.setPadding(new Insets(10, 10, 10, 10));
+        buttonHBox.setAlignment(Pos.CENTER);
+        buttonHBox.setSpacing(20);
+        buttonHBox.getChildren().addAll(sendBtn, closeBtn, helpBtn);
+
+        VBox titleVBox = new VBox(mainlbl);
+        titleVBox.getChildren().add(notiflbl);
+        titleVBox.setAlignment(Pos.CENTER);
+        borderPane.setVisible(false);
+        borderPane.setTop(titleVBox);
+        borderPane.setCenter(gridPane);
+        borderPane.setBottom(buttonHBox);
+        fadeIn.setNode(borderPane);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.setCycleCount(1);
+        fadeIn.setAutoReverse(false);
+        borderPane.setVisible(true);
+        fadeIn.playFromStart();
     }
 
+    /**
+     * Validates credentials from initial stage prior to moving on
+     * a valid email is at least 7 characters long as it must include '@', '.', '.com or .org or .net etc.'
+     * along with text prior to the @ symbol and after it.
+     *
+     * @return: 'True' if all fields are valid.
+     */
     private boolean credentialsCheck() {
         boolean validEmail, validFirstName, validLastName;
         String emailPattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@" +
@@ -123,6 +186,41 @@ public class EmailActivity extends Application {
         return (validEmail && validFirstName && validLastName);
     }
 
+    /**
+     * For this method to work you need to have the mail.jar and activation.jar
+     * for the javamail api to work!
+     */
+    private void sendEmail() {
+        String to = tfTo.getText();
+        String from = tfFrom.getText();
+        String host = "localhost";
+        Properties properties = System.getProperties();
+
+        properties.setProperty("mail.smtp.host", host);
+        //Session session = Session.getDefaultInstace(properties);
+
+        try {
+          /*  MIMEMessage message = new MIMEMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            message.setSubject(tfSubject.getText());
+            message.setText(taMessage.getText());
+
+            Transport.send(message); */
+            notiflbl.setText("Message Sent Successfully!");
+            notiflbl.setTextFill(Color.GREEN);
+            notiflbl.setVisible(true);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * A stage is resized to display all of
+     * it's new elements in the email view
+     * @param stage
+     */
     private void stageResize(Stage stage) {
         Timer windowResizeTimer = new Timer();
         windowResizeTimer.scheduleAtFixedRate(new TimerTask() {
@@ -130,8 +228,8 @@ public class EmailActivity extends Application {
             public void run() {
                 if (stage.getHeight() <= 500) {
                     stage.setHeight(stage.getHeight()+5);
-                } else if (stage.getWidth() <= 500) {
-                    stage.setWidth(stage.getWidth()+10);
+                    if (stage.getWidth() <= 600)
+                        stage.setWidth(stage.getWidth()+10);
                 } else {
                     this.cancel();
                 }
